ba0ec0c1d690ad9154535fe19da2d7a83a31b446
Kane York <kanepyork@gmail.com>
2024-05-18 16:21:08 -0700

    nixos: use npins nixpkgs during rebuild

---
+++ nixops/kane-fwnix/kane-fwnix.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)
+cmd=${1:-switch}
+shift
+
+nixpkgs_pin=$(nix eval --raw -f npins/default.nix nixpkgs)
+export NIX_PATH="nixpkgs=${nixpkgs_pin}:nixos-config=${PWD}/configuration.nix"
+
+# without --fast, nixos-rebuild will compile nix and use the compiled nix to evaluate the config
+sudo nixos-rebuild "$cmd" --fast "$@"
