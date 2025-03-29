b7c0af5bd0ba9397ebbcc7426318d130f80bf1b9
Kane York <kanepyork@gmail.com>
2025-03-29 12:12:13 -0700

    add comma

--- nixops/kane-fwnix/configuration.nix
+++ nixops/kane-fwnix/configuration.nix
@@ -162,6 +162,7 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    comma # nix-shell shortcut
     discord
     dive # container inspector
     fish
